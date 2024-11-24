66fa9b2282bf57b045a4d1063962609cafa9e85e
Roshni Vijay Raisinghani <roshnidv@umich.edu>
2024-11-24 01:15:46 -0500

    Add files via upload

--- js/video.js
+++ js/video.js
@@ -49,28 +49,30 @@ document.querySelector("#mute").addEventListener("click", function () {
 	if (video.muted) {
 		video.muted = false;
 		document.querySelector("#mute").innerHTML = "Mute";
-		console.log("the video is muted");
+		console.log("the video is unmuted");
 	} else if (!video.muted) {
 		video.muted = true;
 		document.querySelector("#mute").innerHTML = "Unmute";
-		console.log("the video is unmuted");
+		console.log("the video is muted");
 	}
 
 });
 
 document.querySelector("#slider").addEventListener("input", function (event) {
 	let sliderValue = event.target.value;
-	document.querySelector("#volume").innerHTML = sliderValue;
+	document.querySelector("#volume").innerHTML = sliderValue + "%";
 	video.volume = sliderValue / 100;
-	console.log("The current volume is " + video.volume + "%");
+	console.log("The current volume is " + video.volume);
 });
 
 document.querySelector("#vintage").addEventListener("click", function () {
-	video.style.filter = "grayscale(100%)";
+	video.classList.replace("video", "oldSchool");
+	console.log("The video is vintage style now");
 });
 
 document.querySelector("#orig").addEventListener("click", function () {
-	video.style.filter = "grayscale(0%)";
+	video.classList.replace("oldSchool", "video");
+	console.log("The video has its original style now");
 });
 
 
